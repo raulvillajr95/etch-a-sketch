f8a216705f138481abe7819e701a989acb4a5e33
Raul <raulvillajr95@gmail.com>
2022-06-15 16:11:15 -0700

    Get opacity to accumalate

--- scripts.js
+++ scripts.js
@@ -9,6 +9,11 @@ function removeAllChildNodes(parent) {
   }
 }
 
+// Get random number
+function randomNum(b) {
+  return Math.floor(Math.random() * b) + 1;
+}
+
 let sizeBtn = document.querySelector('.size')
 
 sizeBtn.addEventListener('click', () => {
@@ -29,10 +34,23 @@ sizeBtn.addEventListener('click', () => {
       for (let j = 1; j <= sizeNum; j++) {
         let divCol = document.createElement('div')
   
-        divCol.setAttribute('style', `height: ${heightPercent}%`)
+        let randomColor = `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`
+
+        divCol.setAttribute('style', `height: ${heightPercent}%; background-color: ${randomColor}; opacity: 0`)
     
         divCol.addEventListener('mouseover', function() {
-          this.classList.add('red')
+          //this.classList.add('red')
+          //this.setAttribute('style', `height: ${heightPercent}%; background-color: rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`)
+          //this.style.backgroundColor = `${randomColor}`
+          // console.log(this.style.opacity)
+          // this.style.opacity = toString(parseFloat(this.style.opacity) + 0.1)
+          // console.log(this.style.opacity)
+          // this.style.opacity = toString(this.style.opacity + 0.1)
+          // console.log(this.style.opacity)
+
+          let opacity = parseFloat(this.style.opacity) + 0.1
+
+          this.style.opacity = `${opacity}`
         })
     
         divRow.appendChild(divCol)
@@ -43,4 +61,10 @@ sizeBtn.addEventListener('click', () => {
 })
 
 /*
+-make it so the new random background color only happens once
+  I could assign it a random color from the start and display none
+  When I mouse over it displays
+-every time mouse is over, the opacity goes up by 10% 
+  assing random color from the start
+  every mouse over the 'a' in rgba gets upped one
 */
